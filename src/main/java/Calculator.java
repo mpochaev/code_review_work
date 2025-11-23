61d3d2c8dcd8b1f50d31ab6376eda8f516a2dd77
mikhail <1028880@edu.rut-miit.ru>
2025-11-23 18:27:19 +0300

    pochaevmt Calculator.java solver

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,25 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        // обычное сложение
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+    public int dif(int a, int b) {
+        // вычитание
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+    public int div(int a, int b) {
+        // целочисленное деление
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+    public int times(int a, int b) {
+        // умножение
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public int solver() {
+        // Решаем выражение: ((10 + 5) - 3) * 2 / 4
+        int sum = add(10, 5);       // 15
+        int diff = dif(sum, 3);     // 12
+        int product = times(diff, 2); // 24
+        return div(product, 4);     // 6
     }
 }
